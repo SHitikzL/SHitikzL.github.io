ff01252428002109ce9a809a64869a06c0469564
weak-imp <KlimovN.code@gmail.com>
2026-06-19 18:18:37 +0300

    Add new post

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_Z1LWJXI from "../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fpractice-for-anxiety-and-stress.md";
+export default new Map([["../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fpractice-for-anxiety-and-stress.md", __ASTRO_IMAGE_IMPORT_Z1LWJXI]]);
+		
---
+++ src/content/posts/practice-for-anxiety-and-stress.md
@@ -0,0 +1,13 @@
+---
+title: Тревожка? Стресс? Ловите практику!
+# description: description text
+showDescription: false
+cover: ../../assets/images/post.practice-for-anxiety-and-stress.jpg
+coverAlt: Изображение девушки в стрессе
+pubDate: 2026-06-18
+video:
+  youtube: "https://www.youtube-nocookie.com/embed/hdO1ijX1OP4?si=0CD1aaP_7uMBBtz7"
+  vk: "https://vkvideo.ru/video_ext.php?oid=151024438&id=456240488&hd=2"
+---
+
+<VideoTabs />
