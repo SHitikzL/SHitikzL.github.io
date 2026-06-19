eef9665c0e706503652419680fa5aa00d7aed694
weak-imp <KlimovN.code@gmail.com>
2026-06-19 18:29:29 +0300

    Add new post

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
+import __ASTRO_IMAGE_IMPORT_Z1x2zox from "../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fburnout.md";
 import __ASTRO_IMAGE_IMPORT_Z1LWJXI from "../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fpractice-for-anxiety-and-stress.md";
-export default new Map([["../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fpractice-for-anxiety-and-stress.md", __ASTRO_IMAGE_IMPORT_Z1LWJXI]]);
+export default new Map([["../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fburnout.md", __ASTRO_IMAGE_IMPORT_Z1x2zox], ["../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fpractice-for-anxiety-and-stress.md", __ASTRO_IMAGE_IMPORT_Z1LWJXI]]);
 		
