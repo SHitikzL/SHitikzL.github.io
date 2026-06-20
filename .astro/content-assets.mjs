93e537c764b835af42e4194cb0c46f62e4efa653
weak-imp <KlimovN.code@gmail.com>
2026-06-20 10:41:33 +0300

    Add new post

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,5 @@
 
-import __ASTRO_IMAGE_IMPORT_Z1x2zox from "../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fburnout.md";
 import __ASTRO_IMAGE_IMPORT_Z1LWJXI from "../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fpractice-for-anxiety-and-stress.md";
-export default new Map([["../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fburnout.md", __ASTRO_IMAGE_IMPORT_Z1x2zox], ["../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fpractice-for-anxiety-and-stress.md", __ASTRO_IMAGE_IMPORT_Z1LWJXI]]);
+import __ASTRO_IMAGE_IMPORT_Z1J2hcT from "../../assets/images/post.what-is-a-trigger.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwhat-is-a-trigger.md";
+export default new Map([["../../assets/images/post.practice-for-anxiety-and-stress.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fpractice-for-anxiety-and-stress.md", __ASTRO_IMAGE_IMPORT_Z1LWJXI], ["../../assets/images/post.what-is-a-trigger.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwhat-is-a-trigger.md", __ASTRO_IMAGE_IMPORT_Z1J2hcT]]);
 		
---
+++ src/content/posts/what-is-a-trigger.md
@@ -0,0 +1,11 @@
+---
+title: Что такое триггер? Всё та же рубрика ответов) 
+# description: description text
+showDescription: false
+cover: ../../assets/images/post.what-is-a-trigger.jpg
+coverAlt: Изображение девушки с визуальными эффектами
+pubDate: 2026-06-20
+video:
+  youtube: "https://www.youtube.com/embed/Hxd0e8N6pAs?si=VVkyRt110Yyfsyc-"
+  vk: "https://vkvideo.ru/video_ext.php?oid=151024438&id=456240495&hd=2"
+---
